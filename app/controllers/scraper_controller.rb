0fc8c6838899fdf7c92901edecd5f6a42097c76b
Kurt Thomas <kurt.thomas34@gmail.com>
2017-04-25 18:06:13 +0000

    can call p elements by index switching to branch to mess around with it

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -1,11 +1,13 @@
 class ScraperController < ApplicationController
   
   class Entry
-    def initialize(title, body)
+    def initialize(title, minititle, body)
       @title = title
+      @minititle = minititle
       @body = body
     end
     attr_reader :title
+    attr_reader :minititle
     attr_reader :body
   end
 
@@ -16,9 +18,10 @@ class ScraperController < ApplicationController
     entries = doc.css('.article')
     @entriesArray = []
     entries.each do |entry|
-      title = entry.css('article--head_title').text #title "What's Going Here?"
-      body = entry.css('p').text #title "What's Going Here?"
-      @entriesArray << Entry.new(title, body)
+      title = entry.css('h2').text #title "What's Going Here?"
+      minititle = entry.css('.icon-goingon').text
+      body = entry.css('p')[0].text #title "What's Going Here?"
+      @entriesArray << Entry.new(title, minititle, body)
       Post.create(title: title, going_on_text: body, publish_date: DateTime.now)
     end
 

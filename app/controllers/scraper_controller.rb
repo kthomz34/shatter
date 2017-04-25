47bd963b600c706de5e28e0048cfdddd102935e7
Kurt Thomas <kurt.thomas34@gmail.com>
2017-04-25 18:46:21 +0000

    seperated out what's going_on_text meant_text care_text

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -1,14 +1,20 @@
 class ScraperController < ApplicationController
   
   class Entry
-    def initialize(title, minititle, body)
+    def initialize(title, going_on_text, meant_text, care_text, bigger_picture_text, publish_date)
       @title = title
-      @minititle = minititle
-      @body = body
+      @going_on_text = going_on_text
+      @meant_text = meant_text
+      @care_text = care_text
+      @bigger_picture_text = bigger_picture_text
+      @publish_date = publish_date
     end
     attr_reader :title
-    attr_reader :minititle
-    attr_reader :body
+    attr_reader :going_on_text
+    attr_reader :meant_text
+    attr_reader :care_text
+    attr_reader :publish_date
+    attr_reader :bigger_picture_text
   end
 
   def scrape_finimize
@@ -19,10 +25,13 @@ class ScraperController < ApplicationController
     @entriesArray = []
     entries.each do |entry|
       title = entry.css('h2').text #title "What's Going Here?"
-      minititle = entry.css('.icon-goingon').text
-      body = entry.css('p')[0].text #title "What's Going Here?"
-      @entriesArray << Entry.new(title, minititle, body)
-      Post.create(title: title, going_on_text: body, publish_date: DateTime.now)
+      going_on_text = entry.css('p')[0].text #going on paragraph
+      meant_text = entry.css('p')[1].text #what does this mean? paragraph
+      care_text = entry.css('p')[2].text #why should I care? paragraph
+      bigger_picture_text = entry.css('p, b').last.text #the bigger picture paragraph
+      publish_date = entry.css('time').text
+      @entriesArray << Entry.new(title, going_on_text, meant_text, care_text, bigger_picture_text, publish_date)
+      Post.create(title: title, going_on_text: going_on_text, meant_text: meant_text, care_text: care_text, bigger_picture_text: bigger_picture_text, publish_date: publish_date)
     end
 
     # We'll just try to render the array and see what happens
